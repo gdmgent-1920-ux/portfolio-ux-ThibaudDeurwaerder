362b65033eb3d548fd2fa9d265594c96b88d40b7
ThibaudDeurwaerder <thibdeur@student.arteveldehs.be>
2020-02-17 13:34:51 +0100

    Updated with my info

--- data.js
+++ data.js
@@ -10,10 +10,10 @@ const data = {
      */
 
     "general": {
-        "firstName": "Andrei",      // fill in with your own first name
-        "lastName": "Tarkovsky",    // fill in with your own last name
-        "class": "1NMD",            // fill in with your class/group e.g. 1AVDa, 1CMOa, 1NMD, ...
-        "studentnumber": "8675309"  // fill in with your student number
+        "firstName": "Thibaud",      // fill in with your own first name
+        "lastName": "Deurwaerder",    // fill in with your own last name
+        "class": "1AVDb",            // fill in with your class/group e.g. 1AVDa, 1CMOa, 1NMD, ...
+        "studentnumber": "105520"  // fill in with your student number
     },
 
     /**
