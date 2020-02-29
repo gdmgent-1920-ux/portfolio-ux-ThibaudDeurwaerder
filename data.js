177c1e03d0796240260791c2b7c61d1cff049a9a
ThibaudDeurwaerder <thibdeur@student.arteveldehs.be>
2020-02-29 18:28:40 +0100

    Update Adobe XD Assessment 1

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/46d3c269-2dcd-4cc5-7d98-b912bf9cc838-30e2/",
                 "target": "_blank"
             }
         ]
