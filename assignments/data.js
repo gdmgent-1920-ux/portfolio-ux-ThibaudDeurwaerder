fd944e1dc96a6000082defface77b469b1632795
ThibaudDeurwaerder <thibdeur@student.arteveldehs.be>
2020-05-26 20:17:03 +0200

    juist 2

---
+++ assignments/data.js
@@ -0,0 +1,100 @@
+/**
+ * Please edit the necessary data to complete your portfolio
+ * Change the assignment URLs (href) if needed
+ */
+
+const data = {
+
+    /**
+     * Change the general data here (this one is used to find out who you are)
+     */
+
+    "general": {
+        "firstName": "Thibaud",      // fill in with your own first name
+        "lastName": "Deurwaerder",    // fill in with your own last name
+        "class": "1AVDb",            // fill in with your class/group e.g. 1AVDa, 1CMOa, 1NMD, ...
+        "studentnumber": "105520"  // fill in with your student number
+    },
+
+    /**
+     * Assignment 1
+     */
+
+    "assignment1": {
+        "buttons": [
+            {
+                "text": "Inspiratieboek",
+                "title": "Mijn PDF met inspiratie (login, functions, result)",
+                "href": "./assignments/assignment1/documents/a1-inspiration.pdf",
+                "target": "_blank"
+            },
+            {
+                "text": "Adobe XD",
+                "title": "Mijn Adobe XD document",
+                "href": "./assignments/assignment1/documents/a1-visual-designs.xd",
+            },
+            {
+                "text": "Adobe XD Prototype",
+                "title": "Mijn Adobe XD prototype (share link)",
+                "href": "https://xd.adobe.com/view/46d3c269-2dcd-4cc5-7d98-b912bf9cc838-30e2/",
+                "target": "_blank"
+            }
+        ]
+    },
+
+    /**
+     * Assignment 2
+     */
+
+    "assignment2": {
+        "buttons": [
+            {
+                "text": "Inspiratieboek",
+                "title": "Mijn PDF met inspiratie",
+                "href": "./assignments/assignment2/documents/a2-inspiration.pdf",
+                "target": "_blank"
+            },
+            {
+                "text": "Adobe XD",
+                "title": "Mijn Adobe XD document",
+                "href": "./assignments/assignment2/documents/a2-visual-designs.xd",
+            },
+            {
+                "text": "Adobe XD Prototype",
+                "title": "Mijn Adobe XD prototype (share link)",
+                "href": "https://xd.adobe.com/view/e276a08e-eea6-44a2-5909-f6e4b28359ab-9447/",
+                "target": "_blank"
+            }
+        ]
+    },
+
+  /**
+  * Assignment 3
+  */
+
+"assignment3": {
+    "buttons": [
+        {
+            "text": "Researchdocument",
+            "title": "Mijn PDF met research",
+            "href": "./assignments/assignment3/documents/a3-research.pdf",
+            "target": "_blank"
+        },
+        {
+            "text": "Adobe XD (met Wireframes)",
+            "title": "Mijn Adobe XD document met de wireframes van mijn applicatie",
+            "href": "./assignments/assignment3/documents/a3-wireframes.xd",
+        },
+        {
+            "text": "Adobe XD (met Visual Designs)",
+            "title": "Mijn Adobe XD document voor de visual designs van mijn applicatie",
+            "href": "./assignments/assignment3/documents/a3-visual-designs.xd",
+        },
+        {
+            "text": "Website",
+            "title": "Mijn website voor opdracht 3",
+            "href": "./assignments/assignment3/website/index.html",
+            "target": "_blank"
+        }
+    ]
+}},
